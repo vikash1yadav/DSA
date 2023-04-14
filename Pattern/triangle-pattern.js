457ccc2b97ebfe44534ec905716e27897deafa75
vikas <vikaskky@gmail.com>
2023-04-14 15:40:52 +0530

    solve dabangg pattern -love babbar

--- Pattern/triangle-pattern.js
+++ Pattern/triangle-pattern.js
@@ -179,6 +179,13 @@ function reverseTriangle(row = 5) {
 }
 // console.log(reverseTriangle(5));
 
+/**
+ 1234554321
+ 1234**4321
+ 123****321
+ 12******21
+ 1********1
+ */
 function dabbangPattern(row = 5) {
     let pattern = "";
 
